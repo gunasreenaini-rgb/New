0294834c76ad2f0e9fd45eeacabed057c6abd6c8
gunasreenaini-rgb <gunasreenayini@gmail.com>
2026-03-04 12:20:46 -0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+
+i= 10
+f= 3.14
+c = 2 + 3j
+s = "Hello people"
+
+my_list = [1, 2, 3, 4]
+my_tuple = (10, 20, 30)
+my_range = range(1, 5)
+
+
+my_dict = {"name": "Yogesh", "age": 25}
+B = True
+
+print("Integer:", i)
+print("Float:", f)
+print("Complex:", c)
+print("String:", s)
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Range:", list(my_range))
+print("Dictionary:", my_dict)
+print("Boolean:", B)
